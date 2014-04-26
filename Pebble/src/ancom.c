dabbe295f46d5c5f9aa54d2e1e2d7f522d6a01ce
zagredam <mzagred1@binghamton.edu>
2014-04-26 10:46:34 -0400

    switch statement

--- Pebble/src/ancom.c
+++ Pebble/src/ancom.c
@@ -7,17 +7,17 @@ Contact* contacts;
 int confirm_flag = -1;
 int num_contacts = 0;
 void out_sent_handler(DictionaryIterator *sent, void *context) {
-   ack = 1;
+   APP_LOG(APP_LOG_LEVEL_DEBUG, "Sending has succeded");
 }
 
 
 void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
-   ack = -1;
+   APP_LOG(APP_LOG_LEVEL_DEBUG, "Sending has failed: %d", reason);
 }
 
 
 void in_dropped_handler(AppMessageResult reason, void *context) {
-   ack = -1;
+   APP_LOG(APP_LOG_LEVEL_DEBUG, "Receiving has failed");
 }
 
 int getFlagValue(){
@@ -39,41 +39,55 @@ int getNumContacts(){
 void in_received_handler(DictionaryIterator *iter, void *context) {
 
           // Check for fields you expect to receive
-          Tuple *text_tuple = dict_find(iter, 0);
-	  num_contacts = text_tuple->value->int32;
-	  if(num_contacts == -1){
-
+          Tuple *mode_tuple = dict_find(iter, 0);
+	  int mode = mode_tuple->value->int32;
+	  switch (mode){
+		  case 0: {
+			  Tuple *name_tuple;
+			  Tuple *number_tuple;
+			  Tuple *val_tuple = dict_find(iter, 1);
+			  confirm_flag = val_tuple->value->int32;
+			  Tuple *num_tuple = dict_find(iter, 2);
+			  num_contacts = num_tuple->value->int32;
+			  contacts = malloc((sizeof(Contact)) * num_contacts);
+			  int contact_count = 0;
+			  APP_LOG(APP_LOG_LEVEL_DEBUG, "Confirm? %d", confirm_flag);
+			  APP_LOG(APP_LOG_LEVEL_DEBUG, "numcontacts %d", num_contacts);
+			  for(int x=0; x<(num_contacts*2); x= x+2){
+				name_tuple = dict_find(iter, x+3);
+				number_tuple = dict_find(iter, x+4);
+				strncpy(contacts[contact_count].name, name_tuple->value->cstring,20);
+				strncpy(contacts[contact_count].number, number_tuple->value->cstring,12);
+			  	APP_LOG(APP_LOG_LEVEL_DEBUG, "Name: %s Number: %s", contacts[contact_count].name, contacts[contact_count].number);
+				contact_count++;
+			  }
+		  	  if(num_contacts > 0){
+		 	 	initialPhoneConnection(contacts[0].name, contacts[0].number);
+		  	  }
+			  return;
+		 }
 	  }
-	  Tuple *name_tuple;
-	  Tuple *number_tuple;
-	  contacts = malloc((sizeof(Contact)) * num_contacts);
-	  int contact_count = 0;
-	  for(int x=0; x<(num_contacts*2); x= x+2){
-		name_tuple = dict_find(iter, x+1);
-		number_tuple = dict_find(iter, x+2);
-		strncpy(contacts[contact_count].name, name_tuple->value->cstring,20);
-		strncpy(contacts[contact_count].number, number_tuple->value->cstring,12);
-	  	APP_LOG(APP_LOG_LEVEL_DEBUG, "Name: %s Number: %s", contacts[contact_count].name, contacts[contact_count].number);
-		contact_count++;
-	  }
-	  Tuple *val_tuple = dict_find(iter, (num_contacts*2)+1);
-	  confirm_flag = val_tuple->value->int32;
-	  APP_LOG(APP_LOG_LEVEL_DEBUG, "Confirm? %d", confirm_flag);
-	  APP_LOG(APP_LOG_LEVEL_DEBUG, "numcontacts %d", num_contacts);
-	  if(num_contacts > 0){
-	 	 initialPhoneConnection(contacts[0].name, contacts[0].number);
-	  }
+	  
 
 }
 
+void placeCall(int position){
+	DictionaryIterator *iter;
+	app_message_outbox_begin(&iter);
+	Tuplet value = TupletInteger(0, 1);
+	dict_write_tuplet(iter, &value);
+	Tuplet numvalue = TupletInteger(1, position);
+	dict_write_tuplet(iter, &numvalue);
+	app_message_outbox_send();
+}
 
 static void fetchContacts(){
 	DictionaryIterator *iter;
 	app_message_outbox_begin(&iter);
 	Tuplet value = TupletInteger(0, 0);
 	dict_write_tuplet(iter, &value);
-	Tuplet value2 = TupletInteger(1, 2);
-	dict_write_tuplet(iter, &value2);
+	//Tuplet value2 = TupletInteger(1, 2);
+	//dict_write_tuplet(iter, &value2);
 	app_message_outbox_send();
 
 }
